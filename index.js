5e92c1f8047c02d169b9cf02614f71ce717607f1
LNFWebsite <LNFWebsite@users.noreply.github.com>
2015-11-05 16:35:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ function streamusJSONInput() {
   var streamusJSON = $("#streamusJSONInput").val();
   var streamusPlaylist = JSON.parse(streamusJSON);
   
-  var videos[0] = streamusPlaylist["title"];
+  var videos;
+  videos[0] = streamusPlaylist["title"];
   
   var streamusVideos = streamusPlaylist["items"];
   for (i = 0; i < streamusVideos.length; i++) {
     var streamusVideo = streamusVideos[i]["video"];
     
-    var video[0] = streamusVideo["title"];
+    var video;
+    video[0] = streamusVideo["title"];
     video[1] = streamusVideo["duration"];
     video[2] = streamusVideo["id"];
     
