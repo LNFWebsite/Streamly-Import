1faa358792738d57d5b1032984ca2aeda57cf18c
LNFWebsite <LNFWebsite@users.noreply.github.com>
2015-11-05 16:33:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 //! License: MIT Adapted (as stated in the LICENSE file)
-var streamusPlaylist;
+
 function streamusJSONInput() {
   var streamusJSON = $("#streamusJSONInput").val();
-  streamusPlaylist = JSON.parse(streamusJSON);
-  console.log(streamusPlaylist);
+  var streamusPlaylist = JSON.parse(streamusJSON);
+  
+  var videos[0] = streamusPlaylist["title"];
+  
+  var streamusVideos = streamusPlaylist["items"];
+  for (i = 0; i < streamusVideos.length; i++) {
+    var streamusVideo = streamusVideos[i]["video"];
+    
+    var video[0] = streamusVideo["title"];
+    video[1] = streamusVideo["duration"];
+    video[2] = streamusVideo["id"];
+    
+    videos.push(video);
+  }
+  console.log(videos);
 }
