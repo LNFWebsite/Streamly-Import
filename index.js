0a70135b0b7390c5799c274fdb9e6c9641d53135
LNFWebsite <LNFWebsite@users.noreply.github.com>
2015-11-05 19:57:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 //! License: MIT Adapted (as stated in the LICENSE file)
 
+function streamusURLInput() {
+  var input = $("#streamusURLInput").val();
+  var regex = /^http(|s):\/\/streamus.com\/share\/playlist\/.*\/.*$/i;
+  
+  input = input.trim();
+  input = input.match(regex);
+  
+  if (input !== null) {
+    window.open(input[0] + "/json");
+    $("#streamusJSONInput").focus();
+  }
+  else {
+    alert("That doesn't appear to be a valid Streamus playlist\n\nTry copying the URL again");
+  }
+}
+
 function streamusJSONInput() {
   var streamusJSON = $("#streamusJSONInput").val();
   try {
